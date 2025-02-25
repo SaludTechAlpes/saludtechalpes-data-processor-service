b508caf2c27dd574641eae59433ba80dba35a56e
Camilo Romero <camiloaromero23@gmail.com>
2025-02-24 22:30:58 -0500

    chore: update list instantiation

--- src/modulos/mapeo/infraestructura/schema/v1/comandos.py
+++ src/modulos/mapeo/infraestructura/schema/v1/comandos.py
@@ -1,11 +1,10 @@
 from pulsar.schema import *
 from dataclasses import dataclass, field
 from src.seedwork.infraestructura.schema.v1.comandos import (ComandoIntegracion)
-from typing import List, Optional
 
 class ComandoMapearDatosPayload(ComandoIntegracion):
     id_imagen = String()
-    etiquetas_patologicas = List(String())
+    etiquetas_patologicas = list(String())
 
 class ComandoMapearDatos(ComandoIntegracion):
     data = ComandoMapearDatosPayload()
