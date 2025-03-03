e4f41d157c14725c39f86e202b642d991b95ba12
Laura Molina <ld.molina11@uniandes.edu.co>
2025-03-03 15:15:33 -0500

    :bug: fixed env variables2

--- src/config/config.py
+++ src/config/config.py
@@ -4,14 +4,7 @@ from dotenv import load_dotenv
 class Config:
     def __init__(self):
         environment = os.getenv('FLASK_ENV')
-
-        if environment == 'local':
-            load_dotenv(dotenv_path='.env.local', override=True)
-        elif environment == 'test':
-            load_dotenv(dotenv_path='.env.test', override=True)
-        else:
-            load_dotenv(dotenv_path='.env', override=True)
-
+        
         self.ENVIRONMENT = environment
         self.APP_NAME = os.getenv('APP_NAME', 'saludtech-data-processor')
         self.PULSAR_HOST = os.getenv('PULSAR_HOST', 'pulsar')
